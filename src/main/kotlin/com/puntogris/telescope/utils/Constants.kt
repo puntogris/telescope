ca265158b4460d9b0f45603e2d72bf4552c4d818
puntogris <rosariodota3@gmail.com>
2025-06-04 23:02:10 -0300

    removed unused code

--- src/main/kotlin/com/puntogris/telescope/utils/Constants.kt
+++ src/main/kotlin/com/puntogris/telescope/utils/Constants.kt
@@ -1,10 +1,8 @@
 package com.puntogris.telescope.utils
 
-const val XML = "xml"
 const val WEBP = "webp"
 const val PNG = "png"
 const val JPEG = "jpeg"
-const val JPG = "jpg"
 
 const val GGUF = "gguf"
 
